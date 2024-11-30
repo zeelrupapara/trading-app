d98f608ac1d2ee4fe575a9b899ad0952c09f645e
zeelrupapara <zeelrupapara@gmail.com>
2024-11-30 21:59:27 +0530

    feat: user table init

---
+++ api/database/migrations/000001_users.up.sql
@@ -0,0 +1,11 @@
+-- +migrate Up
+CREATE TABLE IF NOT EXISTS users (
+    id CHAR (20) PRIMARY KEY,
+    first_name VARCHAR (50) NOT NULL,
+    last_name VARCHAR (50) NOT NULL,
+    email VARCHAR (50) UNIQUE NOT NULL,
+    password VARCHAR (100),
+    roles TEXT,
+    created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
